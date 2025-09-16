b89bd8b6180db7dad27d7f0b223dfeb65bba1a98
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2025-09-16 17:02:17 -0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./auth/authSlice";
+import { blogSlice } from "./blog/blogSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
+    blog: blogSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
