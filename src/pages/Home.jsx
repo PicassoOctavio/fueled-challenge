cf17e858f03998d44f1bf7b1ce3dc6e51d948db0
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2025-09-16 17:03:13 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,29 @@
+import { useEffect } from "react"
+import { useBlogStore } from "../hooks/useBlogStore"
+import { useSelector } from "react-redux";
+import { Post } from "../components/Post/Post";
+
 export const Home = () => {
+  const { getBlogPosts, blogPosts } = useBlogStore();
+  const { user } = useSelector((state) => state.auth)
+  
+  useEffect(() => {
+    getBlogPosts();
+  }, [])
+
   return (
-    <div>Home</div>
+    <>
+      {/* Should only show when user is logged in */}
+      <section className="welcome logged-in">
+        Welcome {user.name}!
+      </section>
+      
+      <div itemScope itemType="https://schema.org/Blog">
+        { blogPosts.map((post) => (
+          <Post key={post.id} {...post} />
+        ))}
+      </div>
+
+    </>
   )
 }
