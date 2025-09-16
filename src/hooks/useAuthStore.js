b1d5b887817c06a121224aaaa2852f0605dde91a
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2025-09-16 10:14:47 -0300

    Create useAuthStore.js

---
+++ src/hooks/useAuthStore.js
@@ -0,0 +1,42 @@
+import { useDispatch } from "react-redux";
+import { fueledApi } from "../api";
+import { clearErrorMessage, onLogin, onLogout } from "../store/auth/authSlice";
+
+export const useAuthStore = () => {
+  const dispatch = useDispatch();
+
+  const startLogin = async ({ username, password }) => {
+    try {
+      const { data } = await fueledApi.post("/jwt-auth/v1/token", {
+        username,
+        password,
+      });
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+
+      dispatch(
+        onLogin({
+          name: data.user_display_name,
+          email: data.user_email,
+          token: data.token,
+        })
+      );
+    } catch (error) {
+      console.log(error);
+      dispatch(onLogout("Credenciales incorrectas"));
+      setTimeout(() => {
+        dispatch(clearErrorMessage());
+      });
+    }
+  };
+
+  const startLogout = () => {
+    localStorage.clear();
+    dispatch(onLogout());
+  };
+
+  return {
+    startLogin,
+    startLogout,
+  };
+};
