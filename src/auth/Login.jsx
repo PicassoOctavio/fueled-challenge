526616ccdf218bc473923a95b438e6adf514bce2
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2025-09-16 10:14:34 -0300

    Login component

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,7 +1,44 @@
+import { useAuthStore } from '../hooks/useAuthStore';
+import { useForm } from '../hooks/useForm';
+import './Login.css'
 
+const loginFormFields = {
+  username: '',
+  password: ''
+}
 
 export const Login = () => {
+
+  const { startLogin } = useAuthStore()
+  const { username, password, onChange } = useForm(loginFormFields);
+
+  const loginSubmit = (event) => {
+    event.preventDefault()
+    startLogin({username, password});
+  }
+
   return (
-    <div>Login</div>
+    <>
+      <h1>
+        Login
+      </h1>
+
+      <div class="login">
+        {/* Connect this form with the WP JWT API */}
+        <form method="post" onSubmit={loginSubmit}>
+          <div>
+            <label for="username">Username</label>
+            <input id="username" type="text" name="username" value={username} onChange={onChange} />
+          </div>
+          <div>
+            <label for="password">Password</label>
+            <input id="password" type="password" name="password" value={password} onChange={onChange} />
+          </div>
+          <div>
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
