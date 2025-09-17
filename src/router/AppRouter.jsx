7ff2bc8ba0cda77c8b84da6d1aca81853d3606ca
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2025-09-17 15:46:04 -0300

    Update AppRouter.jsx

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from 'react'
 import { Route, Routes, Navigate } from "react-router-dom";
 import { Login } from "../auth";
 import {  About, Home } from "../pages";
 import { useAuthStore } from "../hooks/useAuthStore";
 
 export const AppRouter = () => {
-  const { status } = useAuthStore()
+  const { status, checkAuthToken } = useAuthStore();
+
+  useEffect(() => {
+  checkAuthToken();
+
+  const interval = setInterval(() => {
+    checkAuthToken();
+  }, 15 * 60 * 1000);
+
+  return () => clearInterval(interval);
+}, []);
+
   return (
     <Routes>
       {status === 'not-authenticated' 
