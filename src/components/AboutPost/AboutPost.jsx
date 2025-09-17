7be9024d2a292b7a17a1bd423b56c91d7c4174c6
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2025-09-17 15:23:22 -0300

    Update AboutPost.jsx

--- src/components/AboutPost/AboutPost.jsx
+++ src/components/AboutPost/AboutPost.jsx
@@ -5,12 +5,10 @@ export const AboutPost = ({ content }) => {
   const cleanContent = DOMPurify.sanitize(content.rendered);
 
   return (
-    <>
-      <div
-        itemProp="articleBody"
-        className="content"
-        dangerouslySetInnerHTML={{ __html: cleanContent }}
-      />
-    </>
+    <div
+      itemProp="articleBody"
+      className="content"
+      dangerouslySetInnerHTML={{ __html: cleanContent }}
+    />
   )
 }
